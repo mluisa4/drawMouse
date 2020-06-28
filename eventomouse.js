550d45b68123e9b1c97d4d80a4973664db1c2bda
mluisa4 <35712025+mluisa4@users.noreply.github.com>
2020-06-28 18:54:22 -0400

    Terminación de dibujar con mouse
    Terminación exitosa de manejo de eventos con el mouse.  El programa dibuja cuando el mouse esta moviéndose.

--- eventomouse.js
+++ eventomouse.js
@@ -8,18 +8,15 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 console.log(lienzo);
 var dibuja = false;
+var x, y = 0;
 
-var ancho = lienzo.width;
-var alto = lienzo.height;
-var x = ancho/2;
-var y = alto/2;
-var avance = 10;
 
 //funcion que dibuja la linea luego de recibir el evento mousemove
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo1)
 {
   lienzo1.beginPath();
   lienzo1.strokeStyle = color;
+  lienzo1.lineWidht = 1;
   lienzo1.moveTo(xinicial, yinicial);
   lienzo1.lineTo(xfinal, yfinal);
   lienzo1.stroke();
@@ -29,30 +26,26 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo1)
 //funcion que recibe el evento mousedown y activa el dibujo
 function iniciaDibujo(evento)
   {
+  x = evento.offsetX;
+  y = evento.offsetY;
   dibuja = true;
+
   }
 //funcion que dibuja mientras esta activado el mousemove
 function dibujarMientras(evento)
 {
-   if (dibuja == true){
-  //var xin = evento.clientX;
-  //var yin = evento.clientY;
-//  var xfin = (evento.clientX - evento.offsetX);
-  //var yfin = (evento.clientY - evento.offsetY);
-  //dibujarLinea("red", xin, yin, xfin, yfin, lienzo);
-  //console.log(evento);
-  //console.log(dibuja); }
-
-   console.log(evento);
-
- 	 dibujarLinea("Green",x, y,evento.offsetX,evento.offsetY,lienzo);
-	 x = evento.layerX;
-	 y = evento.layerY;
+   if (dibuja == true) {
+   dibujarLinea( "Red", x, y, evento.offsetX, evento.offsetY, lienzo);
+   x = evento.offsetX;
+   y = evento.offsetY;
  }
 }
 
 //funcion que recibe el evento mouseup y desactiva el dibujo
 function paraDibujo(evento)
   {
+    dibujarLinea("Red", x, y, evento.offsetX, evento.offsetY, lienzo);
+    x = 0;
+    y = 0;
     dibuja = false;
   }
